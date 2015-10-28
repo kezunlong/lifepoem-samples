25c8338c56a3a136ee91096bae447f33a5319f71
kezunlong <13761596352@163.com>
2015-10-28 17:29:50 +0800

    AspNetMvcDemo initial checkin

---
+++ AspNetMvcDemo/Controllers/PaginationController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AspNetMvcDemo.Models;
+using Lifepoem.Foundation.Utilities.DBHelpers;
+using Lifepoem.Foundation.Utilities.Helpers;
+
+namespace AspNetMvcDemo.Controllers
+{
+    public class PaginationController : Controller
+    {
+        // GET: Pagination
+        public ActionResult Index(int page = 1)
+        {
+            PagingOption option = GetPagingOption(page, 10);
+            EmployeeManager em = new EmployeeManager();
+            var list = em.SelectEmployees(option);
+            ViewBag.WebPagingOption = new WebPagingOption { CurrentPage = page, ItemsPerPage = 10, TotalItems = option.RecordCount };
+            return View(list);
+        }
+
+        private PagingOption GetPagingOption(int page, int pageSize)
+        {
+            PagingOption option = new PagingOption();
+            option.Start = (page - 1) * pageSize + 1;
+            option.Length = pageSize;
+            option.GetRecordCount = true;
+            return option;
+        }
+    }
+}
---
+++ AspNetMvcDemo/Models/Employee.cs
@@ -0,0 +1,50 @@
+﻿using Lifepoem.Foundation.Utilities.DBHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetMvcDemo.Models
+{
+    /// <summary>
+    /// Test Model
+    /// </summary>
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Telephone { get; set; }
+        public string Remark { get; set; }
+    }
+
+
+    public class EmployeeManager
+    {
+        public List<Employee> SelectEmployees(PagingOption option)
+        {
+            if (option.GetRecordCount)
+            {
+                option.RecordCount = 1000;
+
+                if (option.RecordCount < option.Start)
+                {
+                    option.RecordCount = option.Start;
+                }
+            }
+
+            List<Employee> list = new List<Employee>();
+
+            for (int index = option.Start; index < option.Start + option.Length; index++)
+            {
+                list.Add(new Employee
+                {
+                    Name = "Name " + index.ToString(),
+                    Email = string.Format("Email{0}@163.com", index),
+                    Telephone = "1234567"
+                });
+            }
+
+            return list;
+        }
+    }
+}
